f16a38830bb67c0937cce9729030ed9f364547b3
Florian Klein <florian.klein@free.fr>
2021-11-03 22:51:22 +0100

    separate values from product

--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -1,6 +1,7 @@
 \set ON_ERROR_STOP 1
 \timing on
 
+create extension if not exists tsm_system_rows with schema ext;
 set search_path = pim, ext;
 
 begin;
@@ -14,37 +15,62 @@ insert into family (name, tenant_id)
 select 'family#' || i, tenant_id
 from tenant, generate_series(1, 5) i;
 
+with some_type (type) as (
+    values ('text'), ('number'), ('select')
+)
 insert into attribute (name, type, tenant_id)
-select 'attribute#' || i, 'text', tenant_id
+select 'attribute#' || i, type, tenant_id
+from tenant, generate_series(1, 10) i, some_type;
+
+insert into channel (name, tenant_id)
+select 'channel#' || i, tenant_id
 from tenant, generate_series(1, 10) i;
+
+with some_lang (lang) as (
+    values ('english'), ('french')
+)
+insert into locale (locale_id, name, tenant_id)
+select lang, lang, tenant_id
+from tenant, some_lang;
 
 insert into family_has_attribute (family_id, attribute_id, tenant_id)
 select f.family_id, a.attribute_id, f.tenant_id
 from family f join attribute a using (tenant_id);
 
+insert into product (family_id, tenant_id)
+select f.family_id, f.tenant_id
+from family f, generate_series(1, 1000) i;
 
-create temporary table episode (
-    "episode_id" text primary key,
-    "season_num" text,
-    "episode_name" text,
-    "content_id" text,
-    "release_date" text,
-    "episode_rating" text,
-    "episode_num" text,
-    "description" text,
-    "last_updated" text,
-    "episode_imdb_link" text,
-    "episode_score_votes" text
+drop table if exists sample_episode;
+create table sample_episode (
+    episode_id text primary key,
+    season_num text,
+    episode_name text,
+    content_id text,
+    release_date text,
+    episode_rating text,
+    episode_num text,
+    description text,
+    last_updated text,
+    episode_imdb_link text,
+    episode_score_votes text
 );
-copy episode
+copy sample_episode
 from program 'curl https://raw.githubusercontent.com/raosaif/sample_postgresql_database/master/from_csv/csv_files/episode_list.csv'
 delimiter ',' csv header;
 
-insert into product (family_id, tenant_id, values)
-select f.family_id, f.tenant_id, jsonb_build_object(a.name, (select description || i from episode order by random() limit 1))
-from family f
-join attribute a using (tenant_id),
-generate_series(1, 100) i;
+select create_reference_table('sample_episode');
+
+insert into product_value (product_id, attribute_id, channel_id, locale_id, tenant_id, value)
+select p.product_id, a.attribute_id, c.channel_id, l.locale_id, t.tenant_id,
+(select jsonb_build_object('content', description)->'content' from sample_episode tablesample system_rows(1))
+from tenant t
+join product p using (tenant_id)
+join attribute a using (tenant_id)
+join channel c using (tenant_id)
+join locale l using (tenant_id)
+where a.type = 'text'
+limit 100000;
 
 with c1 as (
     insert into category (name, tenant_id)
--- sql/schema.sql
+++ sql/schema.sql
@@ -3,7 +3,6 @@
 create schema if not exists ext;
 create extension if not exists "uuid-ossp" with schema ext;
 
-
 begin;
 
 drop schema if exists pim cascade;
@@ -41,13 +40,40 @@ create table family_has_attribute (
     foreign key (attribute_id, tenant_id) references attribute (attribute_id, tenant_id) on delete cascade
 );
 
+create table channel (
+    channel_id uuid not null default uuid_generate_v4(),
+    tenant_id uuid not null references tenant (tenant_id) on delete cascade,
+    name text not null,
+    primary key (channel_id, tenant_id)
+);
+
+create table locale (
+    locale_id text not null default uuid_generate_v4(),
+    tenant_id uuid not null references tenant (tenant_id) on delete cascade,
+    name text not null,
+    primary key (locale_id, tenant_id)
+);
+
 create table product (
     product_id uuid not null default uuid_generate_v4(),
     tenant_id uuid not null references tenant (tenant_id) on delete cascade,
     family_id uuid not null,
-    values jsonb not null default '{}',
     primary key (product_id, tenant_id),
     foreign key (family_id, tenant_id) references family (family_id, tenant_id) on delete cascade
+);
+
+create table product_value (
+    product_id uuid not null,
+    attribute_id uuid not null,
+    locale_id text null,
+    channel_id uuid null,
+    tenant_id uuid not null references tenant (tenant_id) on delete cascade,
+    value jsonb not null,
+    primary key (product_id, attribute_id, locale_id, channel_id, tenant_id),
+    foreign key (product_id, tenant_id) references product (product_id, tenant_id) on delete cascade,
+    foreign key (attribute_id, tenant_id) references attribute (attribute_id, tenant_id) on delete cascade,
+    foreign key (locale_id, tenant_id) references locale (locale_id, tenant_id) on delete cascade,
+    foreign key (channel_id, tenant_id) references channel (channel_id, tenant_id) on delete cascade
 );
 
 create table category (
@@ -70,9 +96,12 @@ create table product_in_category (
 
 select create_reference_table('tenant');
 select create_distributed_table('family', 'tenant_id');
+select create_distributed_table('channel', 'tenant_id');
+select create_distributed_table('locale', 'tenant_id');
 select create_distributed_table('attribute', 'tenant_id', colocate_with => 'family');
 select create_distributed_table('family_has_attribute', 'tenant_id', colocate_with => 'family');
 select create_distributed_table('product', 'tenant_id', colocate_with => 'family');
+select create_distributed_table('product_value', 'tenant_id', colocate_with => 'product');
 select create_distributed_table('category', 'tenant_id', colocate_with => 'product');
 select create_distributed_table('product_in_category', 'tenant_id', colocate_with => 'product');
 
